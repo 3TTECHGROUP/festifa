e5e51c7d8baa8f3d1bec7a401cfab7ee10bdb9a2
Hiren <hiren@completeeducationsolutions.uk>
2026-05-04 21:11:29 +0530

    fixis issue build

--- src/types/vite-env.ts
+++ src/types/vite-env.ts
@@ -1,16 +1,13 @@
-import 'vite/client'
+import "vite/client";
 
-interface ImportMetaEnv {
-  readonly VITE_API_URL: string
-  readonly VITE_AWS_S3_BUCKET: string
-  readonly VITE_AWS_REGION: string
-  readonly VITE_AWS_ACCESS_KEY_ID: string
-  readonly VITE_AWS_SECRET_ACCESS_KEY: string
+declare global {
+  interface ImportMetaEnv {
+    readonly VITE_API_URL: string;
+    readonly VITE_AWS_S3_BUCKET: string;
+    readonly VITE_AWS_REGION: string;
+    readonly VITE_AWS_ACCESS_KEY_ID: string;
+    readonly VITE_AWS_SECRET_ACCESS_KEY: string;
+  }
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-interface ImportMeta {
-  readonly env: ImportMetaEnv
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  readonly glob: (pattern: string) => Record<string, { default: any }>
-}
+export {};
